e4235d85d39308e6671edaa37f6c73a622082b42
kdeligt <kdeligt@physnet.uni-hamburg.de>
2021-11-10 21:29:06 +0100

    2d

--- likelihood.cc
+++ likelihood.cc
@@ -25,13 +25,16 @@ double prob(std::vector<int> daten, double mu){
 void likelihood_scan(std::vector<int> daten){
     std::ofstream fout("likelihood.txt");
     std::ofstream fout2("nll.txt");
+    std::ofstream fout3("deltanll.txt");
     for(int i=0;i<=60;i++){
         double mu=i*0.1;
         fout<<mu<<"  "<<prob(daten,mu)<<std::endl;
         fout2<<mu<<"  "<<-2*log(prob(daten,mu))<<std::endl;
+        fout3<<mu<<"  "<<-2*log(prob(daten,mu))+2*log(prob(daten,3.11538))<<std::endl;
     }
     fout.close();
     fout2.close();
+    fout3.close();
 }
 int main() {
     double mu=3.11538;
