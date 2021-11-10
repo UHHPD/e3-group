9c64ede6586b6a5f176bf792abec58c920c56ab2
kdeligt <kdeligt@physnet.uni-hamburg.de>
2021-11-10 03:40:08 +0100

    1a first attempt

--- poisson.cc
+++ poisson.cc
@@ -7,14 +7,20 @@ double poisson(double mu, int k) {
     return 0;
 }
 
-int main() {
-    using namespace std;
-
-
-    ifstream fin("datensumme.txt");
+void counting() {
+    std::vector<int> zaehler(11);
+    std::ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i]+=1;
+
     }
     fin.close();
+    for(int j=0;j<zaehler.size();j++){
+      std::cout<<j<<":"<<zaehler[j]<<std::endl;
+    }
+}
+int main(){
+    counting();
 }
