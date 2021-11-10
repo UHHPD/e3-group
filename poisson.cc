b5cfa310535522811a8ba3371a7f8e6c284e0d3d
kdeligt <kdeligt@physnet.uni-hamburg.de>
2021-11-10 04:22:53 +0100

    excercise 1 attempt

--- poisson.cc
+++ poisson.cc
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
+#include <numeric>
 
 
 double poisson(double mu, int k) {
-    return 0;
+    double P= pow(mu,k)*exp(-mu)/tgamma(k+1);
+    return P;
 }
 
-void counting() {
+std::vector<int> counting() {
     std::vector<int> zaehler(11);
     std::ifstream fin("datensumme.txt");
     int n_i;
@@ -20,7 +23,20 @@ void counting() {
     for(int j=0;j<zaehler.size();j++){
       std::cout<<j<<":"<<zaehler[j]<<std::endl;
     }
+    return zaehler;
+}
+
+void creating_histo(std::vector<int> counts){
+    double mu=3.11538;
+    int sum =std::accumulate(counts.begin(),counts.end(),0);
+    std::ofstream fout("hist.txt");
+    std::ofstream fout2("histpoi.txt");
+    for(int i=0;i<counts.size();i++){
+        fout<<i<<"  "<<counts[i]<<std::endl;
+        fout2<<i<<"  "<<counts[i]<<"  "<<poisson(mu,i)*sum<<std::endl;
+    }
 }
 int main(){
-    counting();
+    std::vector<int> counts=counting();
+    creating_histo(counts);
 }
